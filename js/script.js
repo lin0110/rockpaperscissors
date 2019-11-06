7f5624e5365e14bdcf72582763eddba537c827cd
lin0110 <jiajunlin2016@gmail.com>
2019-11-06 23:38:18 +0000

    added the condition

--- js/script.js
+++ js/script.js
@@ -6,6 +6,33 @@
 /* global $ */
 
 
-
 // DOCUMENT READY FUNCTION BELOW
+$(document).ready(function(){
+    $("#shoot").click(function(){
+        let user = $("input").val();
+        let random = Math.round(Math.random() * 3);
+        console.log(user);
 
+
+        console.log(random);
+        if(user.toLowerCase() === "rock") {
+
+
+
+            if(random === 1){
+                $("#result").html("It's a tie");
+            }
+            else if(random === 2){
+                $("#result").html("You lose!")
+            }
+            else if(random ===3){
+                $("#result").html("You win!")
+            }
+        }
+
+
+
+    });
+
+
+});
