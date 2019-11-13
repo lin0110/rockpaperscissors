e3a9425410cbb2d1d42f3a12dc87bf76da506e8a
lin0110 <jiajunlin2016@gmail.com>
2019-11-13 22:26:22 +0000

    Finish

--- js/script.js
+++ js/script.js
@@ -16,22 +16,65 @@ $(document).ready(function(){
 
         console.log(random);
         if(user.toLowerCase() === "rock") {
-
-
+            $("#userChoice").html("Rock");
 
             if(random === 1){
-                $("#result").html("It's a tie");
+                $("#computerChoice").html("Rock");
+                $("#result").html("It's a tie!");
             }
             else if(random === 2){
-                $("#result").html("You lose!")
+                $("#computerChoice").html("Paper");
+                $("#result").html("You lose!");
             }
             else if(random ===3){
-                $("#result").html("You win!")
+                $("#computerChoice").html("Scissors");
+                $("#result").html("You win!");
             }
         }
 
+        else if(user.toLowerCase() === "paper") {
+            $("#userChoice").html("Paper");
 
+            if(random === 1){
+                $("#computerChoice").html("Rock");
+                $("#result").html("You win!");
+            }
+            else if(random === 2){
+                $("#computerChoice").html("Paper");
+                $("#result").html("It's a tie!");
+            }
+            else if(random ===3){
+                $("#computerChoice").html("Scissors");
+                $("#result").html("You lose!");
+            }
 
+        }
+
+        else if(user.toLowerCase() === "scissors") {
+            $("#userChoice").html("Scissors");
+
+            if(random === 1){
+                $("#computerChoice").html("Rock");
+                $("#result").html("You lose!");
+            }
+            else if(random === 2){
+                $("#computerChoice").html("Paper");
+                $("#result").html("You win!");
+            }
+            else if(random ===3){
+                $("#computerChoice").html("Scissors");
+                $("#result").html("It's a tie!");
+            }
+
+        }
+
+        else{
+            $("#result").html("Please only enter rock, paper, or scissors");
+
+        }
+
+        console.log(user);
+        console.log(random);
     });
 
 
