830c1a2560f24e8b7ebc477569cee3f98b1e777d
lin0110 <jiajunlin2016@gmail.com>
2019-11-20 22:30:24 +0000

    Extention Point System

--- js/script.js
+++ js/script.js
@@ -8,44 +8,55 @@
 
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
+    let userPoint = 0;
+    let computerPoint = 0;
     $("#shoot").click(function(){
         let user = $("input").val();
         let random = Math.random();
         console.log(user);
+        
 
 
         console.log(random);
         if(user.toLowerCase() === "rock") {
             $("#userChoice").html("Rock");
 
-            if(random <= .99){
+            if(random <= .99 && random > .66){
                 $("#computerChoice").html("Rock");
                 $("#result").html("It's a tie!");
             }
-            else if(random <= .66){
+            else if(random <= .66 && random > .33){
                 $("#computerChoice").html("Paper");
                 $("#result").html("You lose! But, let's try again!");
+                computerPoint += 1;
+                $("#computer").text(computerPoint);
             }
             else if(random <= .33){
                 $("#computerChoice").html("Scissors");
                 $("#result").html("You win!");
+                userPoint += 1;
+                $("#user").text(userPoint);
             }
         }
 
         else if(user.toLowerCase() === "paper") {
             $("#userChoice").html("Paper");
 
-            if(random <= .99){
+            if(random <= .99 && random > .66){
                 $("#computerChoice").html("Rock");
                 $("#result").html("You win!");
+                userPoint += 1;
+                $("#computer").text(userPoint);
             }
-            else if(random <= .66){
+            else if(random <= .66 && random > .33){
                 $("#computerChoice").html("Paper");
                 $("#result").html("It's a tie!");
             }
             else if(random <= .33){
                 $("#computerChoice").html("Scissors");
                 $("#result").html("You lose! But, let's try again!");
+                computerPoint += 1;
+                $("#computer").text(computerPoint);
             }
 
         }
@@ -53,21 +64,25 @@ $(document).ready(function(){
         else if(user.toLowerCase() === "scissors") {
             $("#userChoice").html("Scissors");
 
-            if(random <= .99){
+            if(random <= .99 && random > .66){
                 $("#computerChoice").html("Rock");
                 $("#result").html("You lose! But, let's try again!");
+                computerPoint += 1;
+                $("#computer").text(computerPoint);
             }
-            else if(random <= .66){
+            else if(random <= .66 && random > .33){
                 $("#computerChoice").html("Paper");
                 $("#result").html("You win!");
+                userPoint += 1;
+                $("#user").text(userPoint);
             }
             else if(random <= .33){
                 $("#computerChoice").html("Scissors");
                 $("#result").html("It's a tie!");
             }
-
+            
         }
-
+        
         else{
             $("#result").html("Please do not enter other than rock, paper, or scissors.");
 
@@ -75,7 +90,19 @@ $(document).ready(function(){
 
         console.log(user);
         console.log(random);
+        console.log(userPoint);
+        console.log(computerPoint);
     });
+    $("#reset").click(function(){
+        userPoint = 0;
+        computerPoint = 0;
+        $("#computerChoice").text("");
+        $("#userChoice").text("");
+        $("#user").text(userPoint);
+        $("#computer").text(computerPoint);
+        $("#result").text("");
+        
 
+    });
 
 });
